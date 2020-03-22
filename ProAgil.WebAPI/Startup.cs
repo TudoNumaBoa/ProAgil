6cf0c56a47da1c27ba9188b4869b52b90fdeaedf
edson.macedo <edchinguto@gmail.com>
2020-03-22 01:46:49 +0100

    Adicionado Projecto Angular SPA

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -13,45 +13,39 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ProAgil.WebAPI.Data;
 
-namespace ProAgil.WebAPI
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace ProAgil.WebAPI {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
             //---------------O que adicionei -----------------------------------------------
             // using Microsoft.EntityFrameworkCore;
-            services.AddDbContext<DataContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            services.AddDbContext<DataContext> (options =>
+                options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection"))
             );
             //------------------------------------------------------------------------------
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
 
             //app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseMvc ();
 
         }
     }
